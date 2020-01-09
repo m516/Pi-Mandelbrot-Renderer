5f30a18d57e34485d00eafa30a4f434a9205e41d
Micah <micah112358@gmail.com>
2020-01-08 21:59:45 -0600

    Compile, build with SFML for non-RPi systems
    Only tested on Windows. Dialog says sfml DLL isn't found

--- src/PiGPGPU.cpp
+++ src/PiGPGPU.cpp
@@ -15,9 +15,34 @@ int main()
 
 */
 
-#include <stdlib.h>
-#include "QPULib.h"
+//#include <stdlib.h>
+//#include "QPULib.h"
+#include <SFML/Graphics.hpp>
 
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
+
+/*
 void gcd(Ptr<Int> p, Ptr<Int> q, Ptr<Int> r)
 {
     Int a = *p;
@@ -32,7 +57,6 @@ void gcd(Ptr<Int> p, Ptr<Int> q, Ptr<Int> r)
         End
         * r = a;
 }
-
 int main()
 {
     // Construct kernel
@@ -53,3 +77,5 @@ int main()
 
     return 0;
 }
+*/
+
